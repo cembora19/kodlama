bbb0f7bfd1eb8bd6f49dcc44b78cc1aae167bb16
cembora19 <68281307+cembora19@users.noreply.github.com>
2022-02-25 13:47:42 +0300

    Delete pyhton/100python/day17QuizProject directory

--- pyhton/100python/day17QuizProject/day1/ogrendim.py
+++
@@ -1,4 +0,0 @@
-print("welcome to the program")
-city_name=input("which country did you grow up?\n")
-pet_name=input("what is your pet name?")
-print("all names = "+city_name+pet_name)
--- pyhton/100python/day17QuizProject/day4randomisationPythonList/3nestedList.py
+++
@@ -1,4 +0,0 @@
-fruits=["strawberries","nectarines","apples","grapes","peaches","cherries","pears"]
-vegetables=["spinach","kale","tomatoes","celery","potatoes"]
-dirty_dozen=[fruits, vegetables]
-print(dirty_dozen[0][2])
