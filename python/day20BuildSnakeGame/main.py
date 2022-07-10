27a37db92813471ea67afc0873e089707771eaee
cembora19 <cem_bora_5-c@hotmail.com>
2022-07-11 01:05:53 +0300

    eyyo

---
+++ python/day20BuildSnakeGame/main.py
@@ -0,0 +1,29 @@
+import secrets
+from tracemalloc import start, stop
+from turtle import Turtle, Screen, forward, tracer
+import time
+from snake import Snake
+
+screen=Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Welcome to My Snake Game")
+screen.tracer(0)
+
+snake=Snake()
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+game_is_on=True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+screen.exitonclick()
