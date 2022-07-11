892cd72410773ce65680af17341c8a349dacf153
cembora19 <cem_bora_5-c@hotmail.com>
2022-07-11 22:06:42 +0300

    eyyo

--- python/day20BuildSnakeGame/main.py
+++ python/day20BuildSnakeGame/main.py
@@ -1,8 +1,12 @@
 import secrets
 from tracemalloc import start, stop
-from turtle import Turtle, Screen, forward, tracer
+from turtle import Screen, forward, tracer, xcor
 import time
+
+from requests import head
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen=Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +15,8 @@ screen.title("Welcome to My Snake Game")
 screen.tracer(0)
 
 snake=Snake()
+food=Food()
+scoreboard=Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "w")
@@ -23,9 +29,23 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
     #detect collision with food
     if snake.head.distance(food)<15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
+    
+    #detect collision with wall
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() >295  or snake.head.ycor() < -295:
+        game_is_on=False
+        scoreboard.game_over()
+    
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment)<10:
+            game_is_on=False
+            scoreboard.game_over()
 
-screen.exitonclick()
+
+screen.exitonclick() 
